95f9820367bc92f593d10f77ce49eee9580f741f
iinverify <52350677+iinverify@users.noreply.github.com>
2019-07-01 21:07:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,25 @@ if ($_POST["g-recaptcha-response"]) {
     }
 
 if ($response != null && $response->success) {
-
-
-
+    if (isset($_REQUEST['nick_uzytkownika', 'email'])) {
+    
+    $admin_email = "noreply@ycsv.pl";
+    $email = $_REQUEST['email'];
+    $subject = "Weryfikacja nowego użytkownika IINEntertainment";
+    $nazwa_uzytkownika = $_REQUEST['nazwa'];
+    
+    mail($admin_email, "$subject", "$nazwa_uzytkownika", "From:" . $email);
+    
+    echo "Test";
+    }
+    else {
+    ?>
+    
+    <form method="post">
+    Twój adres e-mail <input name="email" type="text" /><br />
+    Twoja nazwa użytkownika Discord <input name="nazwa" type="text" />
+    <input type="submit" value="Zweryfikuj" />
+    
+<?php
+}
 ?>
